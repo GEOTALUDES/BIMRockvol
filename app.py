5c46e9687ff6bec326da185e2ee1491845d21ae7
GEOTALUDES <caterineyepezarmas@gmail.com>
2024-07-18 06:21:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+#set the title
+st.title ('Detección Automática del Volumen de Rocas')
+
+#set header
+st.header ('Please upload an image')
+
+#Upload file
+st.file_uploader ('',type=['png','jpg'.'jpeg'])
+
+#Load the model
